e793200826b859fe97060daf0235241f9e70bfed
kannkyo <15080890+kannkyo@users.noreply.github.com>
2020-09-04 22:09:14 +0900

    Add change-wallpapers.sh

---
+++ change-wallpapers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Specify the path to the desktop background image file
+picture=`find . | grep jpg | sort -R | tail -n 1`
+root=`pwd`
+picture_uri=file://$root/$picture
+
+# Specify one of the rendering options for the background image:
+# 'none', 'wallpaper', 'centered', 'scaled', 'stretched', 'zoom', 'spanned'
+picture_options=zoom
+
+gsettings set org.gnome.desktop.background picture-options $picture_options
+gsettings get org.gnome.desktop.background picture-options
+
+gsettings set org.gnome.desktop.background picture-uri $picture_uri
+gsettings get org.gnome.desktop.background picture-uri
+
