5479147685b644a06ff2f98c64e8adb280d6875c
kannkyo <15080890+kannkyo@users.noreply.github.com>
2020-09-04 23:10:31 +0900

    Fix failed to commit changes to dconf

--- change-wallpapers.sh
+++ change-wallpapers.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd `dirname $0`
+
+# To change wallpaper without X11
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-)
+
 # Specify the path to the desktop background image file
 picture=`find . | grep -E "jpg|jpeg|png|tiff|tif|JPG|JPEG|PNG|TIFF|TIF" | sort -R | tail -n 1`
 root=`pwd`
@@ -8,6 +14,9 @@ picture_uri=file://$root/$picture
 # Specify one of the rendering options for the background image:
 # 'none', 'wallpaper', 'centered', 'scaled', 'stretched', 'zoom', 'spanned'
 picture_options=zoom
+
+gsettings set org.gnome.desktop.background draw-background false
+gsettings get org.gnome.desktop.background draw-background
 
 gsettings set org.gnome.desktop.background picture-options $picture_options
 gsettings get org.gnome.desktop.background picture-options
